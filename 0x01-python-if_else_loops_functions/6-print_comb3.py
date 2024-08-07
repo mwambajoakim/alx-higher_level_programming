1d4e172e0ce1ae94ee04ee785179cac9461ffad4
mwamba <mwambajoakim@gmail.com>
2024-08-07 17:39:41 +0300

    Print different combination of numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Print any combination of two numbers
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print(f"{i}{j}")
+        else:
+            print(f"{i}{j}", end=", ")
