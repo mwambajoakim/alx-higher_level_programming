5a52ee8f7b7671cd50b5ad4e88c8c4d6a83c8398
mwamba <mwambajoakim@gmail.com>
2025-04-26 15:27:15 +0300

    Make an executable file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,3 @@
-# Program that prints alphabet in lowercase
-#
-# No new line at the end
-
-def main():
-
-    for i in range(97, 123):
-        print(chr(i), end="")
-
-
-main()
+#!/usr/bin/python3
+for i in range(97, 123):
+    print(chr(i), end="")
