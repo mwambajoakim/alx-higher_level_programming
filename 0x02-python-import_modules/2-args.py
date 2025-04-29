7f376dd280246f5bc01307a0ed8807f1c22c0e3f
mwamba <mwambajoakim@gmail.com>
2025-04-29 15:59:30 +0300

    Add colon

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,9 @@ if (len(argv) == 1):
     print("{} arguments.".format(length - 1))
 elif (len(argv) >= 2):
     if (len(argv) == 2):
-        print("{} argument\n{}: {}"
+        print("{} argument:\n{}: {}"
               .format(length - 1, length - 1, argv[length - 1]))
     else:
-        print("{} arguments".format(length - 1))
+        print("{} arguments:".format(length - 1))
         for i in range(1, length):
             print("{}: {}".format(i, argv[i]))
-print()
