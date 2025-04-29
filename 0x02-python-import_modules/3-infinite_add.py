a1692b0689e253d0c95c3053f1c7a5a376356a04
mwamba <mwambajoakim@gmail.com>
2025-04-29 16:44:54 +0300

    Sum up command line arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	from sys import argv
+    from sys import argv
+sum = 0
+length = len(argv)
+if (length == 1):
+    sum += length - 1
+for i in range(1, length):
+    sum += int(argv[i])
+print("{}".format(sum))
