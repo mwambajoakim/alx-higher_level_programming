44ad1998f5ac154dd54871c6450f6c89954ef4fa
mwamba <mwambajoakim@gmail.com>
2025-05-01 20:38:46 +0300

    Print integers in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Function that prints a list of integers
+
+        Args:
+            my_list: List of integers.
+    """
+    i = 0
+    length = len(my_list)
+    while (i < length):
+        print("{}".format(my_list[i]))
+        i += 1
