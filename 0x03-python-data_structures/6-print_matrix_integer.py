18cf921e6797d34d208e867f385fbb16f7ea2cd7
mwamba <mwambajoakim@gmail.com>
2025-05-05 14:48:23 +0300

    Print a matrix of numbers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers.
+
+        Args:
+            matrix: List of elements.
+
+        Return:
+            List of integers
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j == (len(matrix[i]) - 1):
+                print("{:d}".format(matrix[i][j]), end="")
+            else:
+                print("{:d}".format(matrix[i][j]), end=" ")
+        print()
