b139c839279cf98ee0e7876156b5211afbab9650
mwamba <mwambajoakim@gmail.com>
2025-05-05 17:03:04 +0300

    Return length and first char of tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Returns length and first character of tuple
+
+        Args:
+            sentence: Tuple with characters
+
+        Return:
+            Length of tuple
+            First character of tuple
+    """
+    length = len(sentence)
+
+    return length, sentence[0]
