e9198f9a7114621140c848389c8932afee4bf954
mwamba <mwambajoakim@gmail.com>
2025-05-05 21:49:05 +0300

    Delete element in a list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """Deletes an element at position idx
+
+        Args:
+        my_list: List of elements.
+        idx: Position of element.
+
+        Return:
+        None
+    """
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    length = len(my_list)
+    i = 0
+    while i < length:
+        if i == idx:
+            del my_list[i]
+        i += 1
+    return my_list
+        
