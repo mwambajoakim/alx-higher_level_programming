a555a7587defcfe5b00f6151ed1caa4117ff2e09
mwamba <mwambajoakim@gmail.com>
2025-05-07 23:28:14 +0300

    Reduce lines to 3

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -9,5 +9,4 @@ def multiply_list_map(my_list=[], number=0):
         Return:
             New list after multiplication
     """
-    new_list = list(map(lambda x: x * number, my_list))
-    return new_list
+    return list(map(lambda x: x * number, my_list))
