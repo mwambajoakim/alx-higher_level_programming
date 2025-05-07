b0a69e2793fe9bae16472d932861dbb72b967069
mwamba <mwambajoakim@gmail.com>
2025-05-07 20:28:44 +0300

    Multiply values by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """Return new dictionary with values multiplied
+        by 2
+
+        Args:
+            a_dictionary: A dictionary
+
+        Return:
+            New dictionary
+    """
+    new_dictionary = {}
+
+    for key, value in a_dictionary.items():
+        value = value * 2
+        new_dictionary[key] = value
+    return new_dictionary
