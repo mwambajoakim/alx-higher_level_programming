cd6be0aee637e3f770bd0d9e8219eef07c6e7c74
mwamba <mwambajoakim@gmail.com>
2025-05-07 22:52:22 +0300

    Return highest score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """Returns the highest score
+
+        Args:
+            a_dictionary: A dictionary
+
+        Return:
+            None if dictionary has no scores
+            Highest score otherwise
+    """
+    if a_dictionary is None:
+        return None
+    max_score = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if max_score == value:
+            return key
