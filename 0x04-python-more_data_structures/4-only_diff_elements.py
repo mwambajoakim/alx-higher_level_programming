cae21222a133ff033318f2c60c574907498602c4
mwamba <mwambajoakim@gmail.com>
2025-05-07 16:28:19 +0300

    Return elements in one set

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+	"""Returns elements not similar in both sets
+
+		Args:
+			set_1: First set
+			set_2: Second set
+
+		Return:
+			Unique elements
+	"""
+	diff_elements = set_1 ^ set_2
+	return diff_elements
