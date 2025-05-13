0b894517b2be438ed9b984a8986ec01182ed4710
mwamba <mwambajoakim@gmail.com>
2025-05-13 13:02:14 +0300

    Describe function

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Prints an integer with format specified
+
+        Args:
+            value: Vvalue to print
+
+        Return:
+            None
+    """
+    
