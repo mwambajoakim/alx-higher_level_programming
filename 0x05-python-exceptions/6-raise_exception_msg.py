816ef580c192b32d4d39a876222a57094f2fdc01
mwamba <mwambajoakim@gmail.com>
2025-05-14 16:38:19 +0300

    Convert indentation to spaces

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-	"""Raises a name exception
+    """Raises a name exception
 
-		Args:
-			message: Message to show
+        Args:
+            message: Message to show
 
-		Return:
-			None
-	"""
-	raise NameError(message)
+        Return:
+            None
+    """
+    raise NameError(message)
