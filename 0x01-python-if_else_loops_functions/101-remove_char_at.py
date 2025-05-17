ee2c795e0eb1428ce4b83c98290c648ff28cf89b
mwamba <mwambajoakim@gmail.com>
2025-05-17 16:03:40 +0300

    Add the appropriate characters to empty string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+	"""Removes a character at n
+
+		Args:
+			str: tring of characters
+			n: Index at which to remove
+
+		Return:
+			None
+	"""
+	cpy_str = str[:]
+	length = len(cpy_str)
+	new_str = ""
+
+	for i in range(length):
+		if i != n:
+			new_str += cpy_str[i]
+	return new_str
