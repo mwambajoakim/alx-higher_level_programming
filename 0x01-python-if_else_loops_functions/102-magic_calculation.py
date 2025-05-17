0bbe4605cfbfb1626acf822b25a4c4d2dba5a31e
mwamba <mwambajoakim@gmail.com>
2025-05-17 15:49:44 +0300

    From bytecode to python

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Does what bytecode looks like
+
+        Args:
+            a: First argument
+            b: Second argument
+            c: Third argument
+
+        Return:
+    """
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - c
