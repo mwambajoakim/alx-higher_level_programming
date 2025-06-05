a223c5c848cf3dec780b82054b7e2d0f561625e3
mwamba <mwambajoakim@gmail.com>
2025-06-05 18:21:15 +0300

    Remove repetitive code

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,15 +12,8 @@ def add_integer(a, b=98):
         Return:
             Sum of a and b
     """
-    int_a = a
-    int_b = b
-
     if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if b is None or not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        int_a = int(a)
-    if isinstance(b, float):
-        int_b = int(b)
-    return (int_a + int_b)
+    return int(a) + int(b)
