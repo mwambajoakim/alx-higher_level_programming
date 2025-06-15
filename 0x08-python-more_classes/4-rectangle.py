8b936431bac78d05579ae4cfee615bb074a3dba8
mwamba <mwambajoakim@gmail.com>
2025-06-15 20:23:01 +0300

    Return rrectangle for eval

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -63,5 +63,5 @@ class Rectangle:
         return string.strip()
 
     def __repr__(self):
-        return "Rectangle(" + str(self.__width) + ",\
-              " + str(self.__height) + ")"
+        return "Rectangle(" \
+        "" + str(self.__width) + ", " + str(self.__height) + ")"
