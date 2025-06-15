f538458ef64c7a79573ac66088167d8249e6a268
mwamba <mwambajoakim@gmail.com>
2025-06-15 18:42:52 +0300

    Print a rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,6 +53,8 @@ class Rectangle:
 
     def __str__(self):
         string = ""
+        if self.__width == 0 or self.__height == 0:
+            return string
         for i in range(self.__height):
             for j in range(self.__width):
                 string += "#"
