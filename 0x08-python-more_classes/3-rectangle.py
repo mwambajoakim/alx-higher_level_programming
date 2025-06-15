ce1708ad79ab3510999e20ebe395684eb0732a46
mwamba <mwambajoakim@gmail.com>
2025-06-15 18:41:19 +0300

    Print a rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Defines a reectangle, its area and perimeter"""
+
+
+class Rectangle:
+    """Creates a rectangle
+
+        Args:
+            width: Rectangle width
+            height: Rectangle height
+
+        Return:
+            Perimeter and area of rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        per = 0
+        if self.__width == 0 or self.___heigth == 0:
+            per = 0
+        else:
+            per = 2 * (self.__width + self.__height)
+        return per
+
+    def __str__(self):
+        string = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string += "#"
+            string += "\n"
+        return string.strip()
