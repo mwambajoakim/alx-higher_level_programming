5410e719c84b07a8147f1d839e922466b751b87a
mwamba <mwambajoakim@gmail.com>
2025-06-18 20:47:32 +0300

    Return list of attributes

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module of a function that returns methods and
+attributes of a class"""
+
+
+def lookup(obj):
+    """Returns list of available attributes and methods"""
+    list = []
+    for attr in obj:
+        list.append(attr)
+    return list
