a33c2d1af8dcb239412c2732ddd90b3e40f47dbf
mwamba <mwambajoakim@gmail.com>
2025-06-19 20:31:14 +0300

    Sort list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Inherit a class which sorts a list"""
+
+
+class MyList(list):
+    """Receives a list and sorts it
+
+        Args:
+            list: Clas list
+
+        Methods:
+            print_sorted: Sorts a list
+
+        Return:
+            A sorted list
+    """
+    def print_sorted(self):
+        print("{}".format(self.sort()))
+
