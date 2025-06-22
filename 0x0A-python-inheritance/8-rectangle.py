39e83b1d47b8bb0caef9a245d9a024c6f35f822b
mwamba <mwambajoakim@gmail.com>
2025-06-22 21:59:49 +0300

    Resolve pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,17 +2,18 @@
 """Creates Rectangle which inherits from BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-	"""Creates a Rectangle that inherits from BaseGeometry
 
-		Args:
-			width: Width of the rectangle
-			height: Height of the rectangle
-	"""
-	def __init__(self, width, height):
-		if width < 0 and type(width) is not int:
-			super().integer_validator("width", width)
-		if height < 0 or type(height) is not int:
-			super().integer_validator("height", height)
-		self.__width = width
-		self.__height = height
+class Rectangle(BaseGeometry):
+    """Creates a Rectangle that inherits from BaseGeometry
+
+        Args:
+            width: Width of the rectangle
+            height: Height of the rectangle
+    """
+    def __init__(self, width, height):
+        if width < 0 and type(width) is not int:
+            super().integer_validator("width", width)
+        if height < 0 or type(height) is not int:
+            super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
