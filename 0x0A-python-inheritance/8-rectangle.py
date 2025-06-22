cd195a57b235f1b6f883a6c26fbe6314f9aedaf7
mwamba <mwambajoakim@gmail.com>
2025-06-22 22:01:21 +0300

    Must be greater than zero

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,9 +11,9 @@ class Rectangle(BaseGeometry):
             height: Height of the rectangle
     """
     def __init__(self, width, height):
-        if width < 0 and type(width) is not int:
+        if width <= 0 and type(width) is not int:
             super().integer_validator("width", width)
-        if height < 0 or type(height) is not int:
+        if height <= 0 or type(height) is not int:
             super().integer_validator("height", height)
         self.__width = width
         self.__height = height
