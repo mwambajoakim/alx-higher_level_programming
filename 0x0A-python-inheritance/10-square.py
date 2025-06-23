eddaaac5fe3b7b0071b063a4e00a8d10bd2ab184
mwamba <mwambajoakim@gmail.com>
2025-06-23 16:29:31 +0300

    Validate size

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,6 +14,7 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         super().__init__("size", size)
+        super().__init__("size", size)
         self.__size = size
 
     def area(self):
