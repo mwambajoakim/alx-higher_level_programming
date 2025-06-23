bb64122cc79e9de0540295889814b23b4703f650
mwamba <mwambajoakim@gmail.com>
2025-06-23 16:22:09 +0300

    Define square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr//bin/python3
+"""Creates a square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square that inherits from Rectangle
+
+        Args:
+            size: Size of the square
+
+        Return:
+            area: Area of the square
+    """
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
