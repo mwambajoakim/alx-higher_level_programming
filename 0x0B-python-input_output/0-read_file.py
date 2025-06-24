7442a25b2b095461ccd50839aa8377b5bafbf568
mwamba <mwambajoakim@gmail.com>
2025-06-24 22:34:11 +0300

    Read a file to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Opens a file for reading"""
+
+
+def read_file(filename=""):
+    """Opens a file for reading and prints to stdout
+
+        Args:
+            filename: Name of the file
+
+        Return:
+            None
+    """
+    with open(filename, 'r', encoding="UTF-8") as read_file:
+        print("{}".format(read_file.read()))
