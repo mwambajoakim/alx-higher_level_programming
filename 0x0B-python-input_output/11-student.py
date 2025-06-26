2261b53fec9b02c99b98a8b30bf474a1c2427b96
mwamba <mwambajoakim@gmail.com>
2025-06-26 19:14:11 +0300

    Replace all attributes of instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Creates a class Student"""
+
+
+class Student:
+    """Instantiates class Student using first name, last name and age
+
+        Args:
+            first_name: First name of student
+            last_name: Last name of student
+            age: Age of student
+
+        Return:
+            Dictionary representation of student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        return {k: self.__dict__[k] for k in attrs if k in self.__dict__}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
