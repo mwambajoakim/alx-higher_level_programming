d75aea97de5378ce5764a7f419b6c13261604981
mwamba <mwambajoakim@gmail.com>
2025-06-27 17:46:00 +0300

    Document rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,16 @@ Base = __import__('base').Base
 
 
 class Rectangle(Base):
+    """Defines a rectangle
+        Args:
+            width: Width of rectangle
+            height: Height of rectangle
+            x: horizontal justification
+            y: vertical justification
+
+        Return:
+            area: Area of the rectangle
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.__width = width
