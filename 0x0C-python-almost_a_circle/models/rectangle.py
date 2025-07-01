f13a6678aa094d83da454f2b27fa6827bd66b4dc
mwamba <mwambajoakim@gmail.com>
2025-07-01 17:03:11 +0300

    Remove extra space

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,5 +82,5 @@ class Rectangle(Base):
 
     def __str__(self):
         return f"[Rectangle]\
-                ({self.id}) {self.__x}/{self.__y}\
+            ({self.id}) {self.__x}/{self.__y}\
                 - {self.__width}/{self.__height}"
