53388f864b4eed1319867beeedab53a19b03e34e
mwamba <mwambajoakim@gmail.com>
2025-07-03 16:22:30 +0300

    Define getter and setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,7 +25,7 @@ class Square(Rectangle):
 
     @property
     def size(self):
-        return self._size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -33,4 +33,5 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self._size = value
+        self.width = value
+        self.height = value
