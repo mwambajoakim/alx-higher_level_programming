0dee6e93e201fa57deb0e5c72e44efe060e2e0d0
mwamba <mwambajoakim@gmail.com>
2025-07-03 15:48:36 +0300

    Inherit rectangle properties

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creates a square"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Returns a square that inherits from Rectangle
+
+        Args:
+            size: size of the square
+            x: horizontal justification
+            y: vertical justification
+            id: ID of the instance
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        a = self.id
+        b = self.x
+        c = self.y
+        d = self.size
+        return f"[Square] ({a}) {b}/{c} - {d}"
