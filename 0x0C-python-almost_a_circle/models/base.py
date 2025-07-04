2516e93316cb1707be08cf87fcb47dea7484a9da
mwamba <mwambajoakim@gmail.com>
2025-07-04 09:32:32 +0300

    Represent dictionary lists as json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Manage id attribute"""
+import json
 
 
 class Base:
@@ -17,3 +18,10 @@ class Base:
         else:
             __class__.__nb_objects += 1
             self.id = __class__.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
