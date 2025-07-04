9375072bb1f527904937942224b956dbcacd9da4
mwamba <mwambajoakim@gmail.com>
2025-07-04 13:08:15 +0300

    Test base class

--- 0x0C-python-almost_a_circle/tests/test_base-0.py
+++ 0x0C-python-almost_a_circle/tests/test_base-0.py
@@ -3,7 +3,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from models.base import Base
+from ..models.base import Base
 import unittest
 
 
