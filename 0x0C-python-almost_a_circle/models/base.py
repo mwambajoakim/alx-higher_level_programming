9f4596e0f6de5fa79f020a3d5a76478b5a66da29
mwamba <mwambajoakim@gmail.com>
2025-07-04 11:32:09 +0300

    Save list of dictionaries to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,22 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Returns a list of JSON dictionaries"""
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string representation into a file"""
+        filename = f"{cls.__name__}.json"
+        list_dictionaries = []
+        with open(filename, 'w', encoding="UTF-8") as json_file:
+            if list_objs is None:
+                json_file.write("[]")
+            else:
+                for val in list_objs:
+                    list_dictionaries.append(val.to_dictionary())
+                json_str = cls.to_json_string(list_dictionaries)
+                json_file.write(json_str)
