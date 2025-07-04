a61d0f05d2dd1803ccae4925123c8fd085459dcd
mwamba <mwambajoakim@gmail.com>
2025-07-04 16:20:45 +0300

    Test base

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Test Base class"""
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from models.base import Base
+import unittest
+
+
+class TestBase(unittest.TestCase):
+    def test_id_None(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+    def test_with_ID(self):
+        b1 = Base(4)
+        self.assertEqual(b1.id, 4)
+    def test_increment(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
