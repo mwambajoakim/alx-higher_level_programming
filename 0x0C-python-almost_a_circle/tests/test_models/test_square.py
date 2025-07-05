c90f6a474179af50e148b4fc86ed7cc8603e97e3
mwamba <mwambajoakim@gmail.com>
2025-07-05 23:38:00 +0300

    Test size

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Tests the class Square"""
+import os
+import sys
+from models.square import Square
+import unittest
+
+class TestSquare(unittest.TestCase):
+
+	def testSize(self):
+		sq = Square(3)
+		self.assertEqual(sq.size, 3)
