6c4b65168c2762d320ddc15ba69926b8dc88af21
mwamba <mwambajoakim@gmail.com>
2025-07-05 23:41:37 +0300

    Test id

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -10,3 +10,8 @@ class TestSquare(unittest.TestCase):
 	def testSize(self):
 		sq = Square(3)
 		self.assertEqual(sq.size, 3)
+		self.assertEqual(sq.id, 2)
+
+	def testID(self):
+		sq = Square(2, 6)
+		self.assertEqual(sq.id, 1)
