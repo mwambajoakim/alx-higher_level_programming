c186b7f74df225fc0ba865cf6523301ac386450f
mwamba <mwambajoakim@gmail.com>
2025-07-06 20:50:39 +0300

    Resolve pycodestyle

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -7,25 +7,25 @@ import unittest
 
 class TestSquare(unittest.TestCase):
 
-	def testSize(self):
-		sq = Square(3)
-		self.assertEqual(sq.size, 3)
-		self.assertEqual(sq.id, 2)
+    def testSize(self):
+        sq = Square(3)
+        self.assertEqual(sq.size, 3)
+        self.assertEqual(sq.id, 15)
 
-	def testID(self):
-		sq = Square(2, 6)
-		self.assertEqual(sq.id, 1)
+    def testID(self):
+        sq = Square(2, 6)
+        self.assertEqual(sq.id, 14)
 
-	def testAssignedId(self):
-		sq = Square(2, 0, 0, 12)
-		self.assertEqual(sq.id, 12)
+    def testAssignedId(self):
+        sq = Square(2, 0, 0, 12)
+        self.assertEqual(sq.id, 12)
 
-	def testSizeTypeError(self):
-		with self.assertRaises(TypeError):
-			sq = Square("3")
-			sq = Square([8])
+    def testSizeTypeError(self):
+        with self.assertRaises(TypeError):
+            sq = Square("3")
+            sq = Square([8])
 
-	def testSizeValueError(self):
-		with self.assertRaises(ValueError):
-			sq = Square(0)
-			sq = Square(-4)
+    def testSizeValueError(self):
+        with self.assertRaises(ValueError):
+            sq = Square(0)
+            sq = Square(-4)
