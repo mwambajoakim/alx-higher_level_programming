c2d5c57b64fec925793059c9b2f0e4701f52b079
mwamba <mwambajoakim@gmail.com>
2025-09-10 17:16:03 +0300

    Create a table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Creates a table in particular database
+-- Script will not fail even if table exists
+
+CREATE TABLE IF NOT EXISTS first_table(
+       id INT,
+       name VARCHAR(256)
+       );
