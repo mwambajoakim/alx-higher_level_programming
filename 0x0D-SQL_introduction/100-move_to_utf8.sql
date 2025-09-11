9f05229ac679ec9b35da045a0795c0ae5ec2c762
mwamba <mwambajoakim@gmail.com>
2025-09-11 16:35:02 +0300

    Change metacharset

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,21 @@
+-- Converts a database to UTF-8
+ALTER DATABASE
+  hbtn_0c_0
+CHARACTER SET
+  utf8mb4
+COLLATE
+  utf8mb4_unicode_ci;
+
+ALTER TABLE
+  first_table
+CHARACTER SET
+  utf8mb4
+COLLATE
+  utf8mb4_unicode_ci;
+
+ALTER COLUMN
+  name
+CHARACTER SET
+  utf8mb
+COLLATE
+  utf8mb_unicode_ci;
