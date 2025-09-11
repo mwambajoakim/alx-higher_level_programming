3488a6371f5274215c1c4baa5ea039523ab82bd6
mwamba <mwambajoakim@gmail.com>
2025-09-11 20:26:23 +0300

    Remove import

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,5 @@
--- import table to hbtn_0c_0
-mysql -u root -p hbtn_0c_0 < temperatures.sql
-
-SELECT city, AVG(value) as avg_temp
+-- Display average temperature by city
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
