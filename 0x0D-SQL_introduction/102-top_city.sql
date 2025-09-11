91bbd52b2d9000efd1f30a2c0d32c077f13b78e6
mwamba <mwambajoakim@gmail.com>
2025-09-11 20:52:22 +0300

    Display max temperature

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
--- Display top 3 cities temperature
+-- Display top 3 cities temperature during July and August
 SELECT city, MAX(value) AS avg_temp
 FROM temperatures
-WHERE month = 7 OR month = 8
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
