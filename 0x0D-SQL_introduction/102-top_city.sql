456652b9d31610ea52ad447359c871029b433432
mwamba <mwambajoakim@gmail.com>
2025-09-11 20:39:46 +0300

    Make alias

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Display top 3 cities temperature
-SELECT city, value
+SELECT city, value AS avg_temp
 FROM temperatures
 WHERE month IN (7, 8)
-ORDER BY value DESC
+ORDER BY avg_temp DESC
 LIMIT 3;
