f63f5af8cc31a527f6e382ac03ffd7beabd72d62
mwamba <mwambajoakim@gmail.com>
2025-09-11 20:37:50 +0300

    Display top 3 cities:July and August

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Display top 3 cities temperature
 SELECT city, value
 FROM temperatures
-WHERE month = 7 and month = 8
-LIMIT 3
-ORDER BY value DESC;
+WHERE month in (7, 8)
+ORDER BY value DESC
+LIMIT 3;
