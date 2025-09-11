908ae2d8164bb7b8d39942c8c6e095a1216c1984
mwamba <mwambajoakim@gmail.com>
2025-09-11 16:51:27 +0300

    Change metacharset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,14 +6,14 @@ COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE
   first_table
-CHARACTER SET
+CONVERT TO CHARACTER SET
   utf8mb4
 COLLATE
   utf8mb4_unicode_ci;
 
 ALTER COLUMN
   name
-CHARACTER SET
+CONVERT TO CHARACTER SET
   utf8mb
 COLLATE
   utf8mb_unicode_ci;
