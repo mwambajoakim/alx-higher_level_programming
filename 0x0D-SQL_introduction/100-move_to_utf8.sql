f96b6f4f6f2e8977e714516d4c7796572c571152
mwamba <mwambajoakim@gmail.com>
2025-09-11 16:42:07 +0300

    Change metacharset

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,8 @@
 -- Converts a database to UTF-8
 ALTER DATABASE
   hbtn_0c_0
-CHARACTER SET
-  utf8mb4
-COLLATE
-  utf8mb4_unicode_ci;
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 ALTER TABLE
   first_table
