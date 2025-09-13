80addfe357c9d32be4eb5639034f3e1f36bdc4db
mwamba <mwambajoakim@gmail.com>
2025-09-13 15:42:45 +0300

    Switch to database

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,17 @@
+-- Switch to hbtn_0c_0 database
+USE hbtn_0c_0
+
 -- Converts a database to UTF-8
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
+-- Convert table to utf8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
+-- Convert name column to utf8
 ALTER TABLE first_table
 MODIFY name VARCHAR(255)
 CHARACTER SET utf8mb4
