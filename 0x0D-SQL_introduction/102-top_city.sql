1ab9e4b8be3ec4cff1277fe9b513b6195dad7ca4
mwamba <mwambajoakim@gmail.com>
2025-09-13 16:05:23 +0300

    Display average temperature

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,5 @@
 -- Display top 3 cities temperature during July and August
-SELECT city, MAX(value) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month IN (7, 8)
 GROUP BY city
