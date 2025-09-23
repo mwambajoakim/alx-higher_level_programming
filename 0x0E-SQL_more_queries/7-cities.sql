d357f52329c6631ca7a39296e6f8e7d511b2f61e
mwamba <mwambajoakim@gmail.com>
2025-09-23 21:02:40 +0300

    Use database

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+USE hbtn_0d_usa;
+
 CREATE TABLE IF NOT EXISTS cities (
        id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
        state_id INT NOT NULL,
