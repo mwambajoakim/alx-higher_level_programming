d72e971b2ea92b9352ff7aacb787f2353c340a3d
mwamba <mwambajoakim@gmail.com>
2025-09-23 17:03:29 +0300

    Show user privileges for localhost

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,6 @@
 -- Show the privileges for users
--- user_0d_1
--- user_0d_2
+-- user_0d_1@localhost
+-- user_0d_2@localhost
 
-SHOW GRANTS FOR 'user_0d_2';
-SHOW GRANTS FOR 'user_0d_2';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
