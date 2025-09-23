089dfdc5ff781f963efffd534a6fd55d1e15a53e
mwamba <mwambajoakim@gmail.com>
2025-09-23 18:14:57 +0300

    Grant select privilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
--- create database hbtn_0d_2
+-- Create database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 --Create user user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2.'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
        IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Give user user_0d_2_pwd select privilege
