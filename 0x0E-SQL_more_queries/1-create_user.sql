119806e0f400fe600fc1c4104141821246884ced
mwamba <mwambajoakim@gmail.com>
2025-09-23 18:06:12 +0300

    Change password

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,7 @@
 -- Grants all privileges to user
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-       IDENTIFIED BY 'User123!';
+       IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES ON *.*
       TO 'user_0d_1'@'localhost';
