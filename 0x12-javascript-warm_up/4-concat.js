5f41f024fec9e4881c774bafff8794eb86f3f64c
mwamba <mwambajoakim@gmail.com>
2025-10-20 19:50:11 +0300

    Concatenate arguments

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (args[0] && args[1]) {
+  console.log(args[0] + ' ' + 'is' + ' ' + args[1]);
+} else if (!args[1]) {
+  console.log(args[0] + ' ' + 'is' + ' undefined');
+} else if (!(args[0] && args[1])) {
+  console.log('undefined is undefined');
+}
