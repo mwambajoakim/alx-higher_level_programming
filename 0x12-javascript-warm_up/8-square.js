cf90165500daab9cdf5559c82185665a6da4f037
mwamba <mwambajoakim@gmail.com>
2025-10-21 14:07:39 +0300

    Print square to stdout

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 const args = process.argv.slice(2);
 const size = parseInt(args[0]);
 
-if (size) {
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < size; i++) {
     console.log('x'.repeat(size));
   }
-} else {
-  console.log('Missing size');
 }
