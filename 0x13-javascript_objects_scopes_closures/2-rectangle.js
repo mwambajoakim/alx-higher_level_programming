d4ef6548bed8b69793749c30110dd12f189e03b6
mwamba <mwambajoakim@gmail.com>
2025-10-26 19:12:47 +0300

    Check if height and width are numbers

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
   constructor (width, height) {
     if (typeof width !== 'number' || typeof height !== 'number' || width <= 0 || height <= 0) {
-      return {};
+      return;
     }
     this.width = width;
     this.height = height;
