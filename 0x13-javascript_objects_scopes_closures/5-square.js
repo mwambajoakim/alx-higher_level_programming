3a342c34005b8b6b95287711dd13fd298ac08b34
mwamba <mwambajoakim@gmail.com>
2025-10-29 11:50:55 +0300

    Class Square: Extend from Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  print () {
+    for (let i = 0; i < this.width; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width = 2 * this.width;
+  }
+}
+module.exports = Square;
