86188f76e5447bafccc6e3f2021fb9159f1d7fd0
mwamba <mwambajoakim@gmail.com>
2025-10-29 12:34:58 +0300

    Class Square: Remove repeat methods

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,15 +4,5 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-
-  print () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  double () {
-    this.width = 2 * this.width;
-  }
 }
 module.exports = Square;
