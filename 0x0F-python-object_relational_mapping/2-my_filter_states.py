4bea39327edc70ec7e62983029a5981348e63e51
mwamba <mwambajoakim@gmail.com>
2025-10-31 22:13:52 +0300

    Get rresults from name given in command line

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name = sys.argv[4] ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
