cab4355db2187aa59c88a7eaf0a8bd5a8d1b4593
mwamba <mwambajoakim@gmail.com>
2025-10-31 20:44:42 +0300

    List all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+import MySQLdb
+import sys
+
+
+""" List all states from a database
+"""
+db = MySQLdb.connect(
+    host=localhost,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    port=3306,
+    db=sys.argv[3]
+    )
+
+cur = db.cursor()
+
+try:
+    cur.execute(SELECT * FROM states ORDER BY id ASC)
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+
+    cur.close()
+    db.close()
+except MySQLdb.error as e:
+    print(e)
