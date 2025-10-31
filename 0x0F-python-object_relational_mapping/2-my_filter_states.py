6b4f22dc0e0840475c7f63477e8e6ad1e2347da9
mwamba <mwambajoakim@gmail.com>
2025-10-31 22:22:49 +0300

    Use binary strict mode

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE name = '{}' "
+        "SELECT * FROM states WHERE BINARY name = '{}' "
         "ORDER BY id ASC".format(sys.argv[4]))
     rows = cur.fetchall()
 
