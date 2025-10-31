0768b40d98fcc993b5637115572a3c0d2fd64168
mwamba <mwambajoakim@gmail.com>
2025-10-31 21:09:29 +0300

    Make executable

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
-        usr=sys.argv[1],
+        user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
         port=3306
