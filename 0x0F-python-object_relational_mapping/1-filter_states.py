382de3cdddb6d683e262d694845e449457d569bc
mwamba <mwambajoakim@gmail.com>
2025-10-31 21:11:43 +0300

    Include id of state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT name FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
 
     for row in rows:
