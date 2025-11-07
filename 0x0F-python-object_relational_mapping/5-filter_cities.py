20b5ca72a1f2db7ac60a29c098e752eb5a717d35
mwamba <mwambajoakim@gmail.com>
2025-11-07 08:44:08 +0300

    Filter by cities through state name

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     rows = cur.fetchall()
 
-    print(", ".join([row[0] for row in rows])
+    print(", ".join([row[0] for row in rows]))
 
     cur.close()
     db.close()
