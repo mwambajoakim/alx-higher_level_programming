c3b0800fb86b6b6b91e96889e8b2b610770482b3
mwamba <mwambajoakim@gmail.com>
2025-11-09 21:19:36 +0300

    Print nothing if no data

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
     first_state = session.query(State).order_by(State.id).first()
 
     if first_state:
-        print(f"{first_state.id}:{first_state.name}")
-    print("Nothing")
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
 
     session.close()
