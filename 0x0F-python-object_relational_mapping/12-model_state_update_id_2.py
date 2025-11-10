00ecd4ae0f493fe3bf30e9d2589871b2b04cc924
mwamba <mwambajoakim@gmail.com>
2025-11-10 21:13:11 +0300

    Change state name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+changes the name of a State
+object from the database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from moodel_state import State, Base
+
+
+if __name__ == "__main__":
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    db_url = f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
+
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    change_name = session.query(State).filter(State.id == 2).first()
+
+    change_name.name = "New Mexico"
+
+    session.add(change_name)
+    session.commit()
+
+    session.close()
