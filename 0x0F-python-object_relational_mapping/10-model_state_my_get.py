88dd2ffbe3c72f622896b400750f3448e16450e3
mwamba <mwambajoakim@gmail.com>
2025-11-10 20:45:11 +0300

    Return state id if exists

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -4,14 +4,14 @@ prints the State object with the name
 passed as argument from the database
 """
 from sqlalchemy import create_engine
-from sqlalchemy.sql import func
 from sqlalchemy.orm import sessionmaker
 from model_state import State, Base
 import sys
 
 
 if __name__ == "__main__":
-    user, passwd, db, state_search = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    user, passwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    state_search = sys.argv[4]
 
     db_url = f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
     engine = create_engine(db_url, pool_pre_ping=True)
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(func.count(State.id).filter(State.name.contains(state_search))).scalar()
+    states = session.query(filter(State.name == state_search)).first()
 
     if states:
         print(states)
