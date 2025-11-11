a98f5cd4f3583061a3101c564d8aeaea9f8b0ef6
mwamba <mwambajoakim@gmail.com>
2025-11-11 16:00:20 +0300

    Refer back to parent state

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from model_state import State, Base
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+    """Creates a table for cities
+
+       Attributes:
+          id: The city id.
+          name: The city name.
+          state_id: Foreign key pointing to state of city.
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
