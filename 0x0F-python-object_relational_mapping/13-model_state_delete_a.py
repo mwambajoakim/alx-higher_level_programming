b48f76332e5bc417a76d8e0d03bd4108d805bc59
mwamba <mwambajoakim@gmail.com>
2025-11-11 09:14:42 +0300

    Delete states that have "a"

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -23,8 +23,7 @@ if __name__ == "__main__":
 
     a_states = session.query(State).filter(State.name.like("%a%")).all()
 
-    for state in a_states:
-        session.delete(state)
-        session.commit()
+    session.delete(a_states)
+    session.commit()
 
     session.close()
