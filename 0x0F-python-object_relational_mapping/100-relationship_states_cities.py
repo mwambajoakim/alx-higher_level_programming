67a53bbd15cd617fcfe0bcf8b8e585de571fe4ba
mwamba <mwambajoakim@gmail.com>
2025-11-11 20:27:20 +0300

    Bind engine with create_all

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     engine = create_engine(db_url, pool_pre_ping=True)
 
-    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
