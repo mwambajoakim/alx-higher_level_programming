2dd07b70f9d2eb611e331373e188c71fa31a9e51
mwamba <mwambajoakim@gmail.com>
2025-11-13 16:45:00 +0300

    Declare Base

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,7 +2,6 @@
 """
 Contains the class definition of a City
 """
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
