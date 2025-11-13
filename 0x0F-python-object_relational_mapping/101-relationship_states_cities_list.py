364020349d5d580ce80f2e012d5e172c40e57628
mwamba <mwambajoakim@gmail.com>
2025-11-13 18:00:41 +0300

    List cities belonging to a state

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -25,11 +25,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    results = session.query(State).order_by(State.id, City.id).all()
+    results = session.query(State).order_by(State.id).all()
 
     for state in results:
+        _ = state.cities
         print(f"{state.id}: {state.name}")
-        for city in state.cities:
-            print(f"    {city.id}: {city.name}")
 
     session.close()
