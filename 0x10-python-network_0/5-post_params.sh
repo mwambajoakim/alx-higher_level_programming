b88116c8cf3321bff1bfb5731538a3e74355154a
mwambajoakim <mwambajoakim@gmail.com>
2025-12-03 14:14:17 +0300

    Specify url

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script sends a POST method to server with variables using curl
-curl -s --request "POST" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
+curl -s --request "POST" -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
