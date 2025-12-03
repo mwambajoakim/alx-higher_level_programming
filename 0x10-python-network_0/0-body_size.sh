528d2f1e6ef5e956ba7038a8e6706cb9b26b716e
mwambajoakim <mwambajoakim@gmail.com>
2025-12-03 09:32:39 +0300

    Remove unnecessary code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # Displays the size of the response body from a url
-if [[ -z "$1" ]]; then
-    echo "Usage $0 url"
-    exit 1
-fi
-
-url="$1"
-
-size=$(curl -s -w "%(size_download)" -o /dev/null "$url")
-echo "$size"
+curl -s -w "%(size_download)" -o /dev/null "$1"
