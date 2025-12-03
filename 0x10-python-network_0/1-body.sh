aa705521e240f571e28a355f55446b9fea1069cd
mwambajoakim <mwambajoakim@gmail.com>
2025-12-03 10:26:36 +0300

    Comment new

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script sends a GET to a server and displays repsonse
+# Script sends a GET to a server and displays responses for 200 status code
 curl -s -L -w "%{http_code}" "$1" -o response_body > status_code; cat response_body
