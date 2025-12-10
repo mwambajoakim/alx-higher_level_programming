9fd0a33409ffa565f4f2aa7262bb838dd3d6d441
mwambajoakim <mwambajoakim@gmail.com>
2025-12-10 16:24:09 +0300

    Print server response

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that requests a url and prints the response"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    req = requests.get(url)
+    print("Body Response:")
+    print(f"\t- type: {req.text}")
+    print(f"\t - content: {req.content}")
