7d05aee34ee293a21c97f15c27d4e569d6c07f20
mwambajoakim <mwambajoakim@gmail.com>
2025-12-10 14:32:06 +0300

    Do not execute when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,12 @@ Script that fetches https://alx-intranet.hbtn.io/status
 """
 from urllib import request
 
-"""
-Fetch contents of url in a context manager
-"""
-url = "https://alx-intranet.hbtn.io/status"
 
-with request.urlopen(url) as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body.decode('utf-8'))
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body.decode('utf-8'))
