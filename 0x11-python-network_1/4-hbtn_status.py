15c1a0d08ef4de165e524bfab3788350855f4356
mwambajoakim <mwambajoakim@gmail.com>
2025-12-10 16:38:48 +0300

    Remove space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     req = requests.get(url)
     print("Body Response:")
-    print(f"\t- type: ", type(req.text))
+    print(f"\t- type:", type(req.text))
     print(f"\t- content: {req.text}")
